8d722219e38c0078c6888b8ab7b1c38b456ad457
unknown <mmahato@manoj-pc.braindigit.local>
2014-04-01 16:36:54 +0545

    First Upload

---
+++ testClass/Service.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Activation;
+
+
+namespace Ser
+{
+    // NOTE: If you change the class name "Service" here, you must also update the reference to "Service" in Web.config and in the associated .svc file.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service : IService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+
+        public string GetUser(string Id)
+        {
+            return new User().GetUser(Convert.ToInt32(Id)).ToString();
+        }
+    }
+
+
+
+    public class User
+    {
+
+        Dictionary<int, string> users = null;
+        public User()
+        {
+            users = new Dictionary<int, string>();
+            users.Add(1, "manoj");
+            users.Add(2, "shyam");
+            users.Add(3, "chandra");
+            users.Add(4, "dinesh");
+        }
+
+        public string GetUser(int Id)
+        {
+            var user = from u in users
+                       where u.Key == Id
+                       select u.Value;
+            return user.ToArray<string>().FirstOrDefault().ToString();
+        }
+
+    }
+}
